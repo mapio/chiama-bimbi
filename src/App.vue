c5a690d60acfdf446b9150bc8bba164bc1c13bf6
Massimo Santini <massimo.santini@gmail.com>
2019-04-07 18:08:29 +0200

    Now the UI seems to be accetpable

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
-  <div id="app">
-    <CallMe />
-  </div>
+  <v-app id="callme">
+    <v-content>
+      <v-container fill-height>
+        <v-layout>
+          <v-flex>
+            <v-card class="elevation-8" style="height: 100%">
+              <CallMe/>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import CallMe from './components/CallMe.vue'
+import CallMe from './components/CallMe'
 
 export default {
-  name: 'app',
-  components: {
-    CallMe
-  }
+  name: 'App',
+  components: { CallMe },
+  data() { return {}}
 }
 </script>
-
-<style>
-#app {
-}
-</style>
--- src/components/CallMe.vue
+++ src/components/CallMe.vue
@@ -1,16 +1,63 @@
 <template>
-  <div class="callme">
-    <textarea v-model="names">
-    </textarea>
-    <button @click="doStart">Inizia!</button>
-    <Container group-name="names" @drop="rap => onDrop(0, rap)" :get-child-payload="itemIndex => getChildPayload(0, itemIndex)">
-        <Draggable v-for="item in toBeCalled" :key="item.id"><span class="name">{{item.name}}</span></Draggable>
-    </Container>
-    <button @click="doCall">Chiama!</button>
-    <Container group-name="names" @drop="rap => onDrop(1, rap)" :get-child-payload="itemIndex => getChildPayload(1, itemIndex)">
-        <Draggable v-for="item in called" :key="item.id"><span class="name">{{item.name}}</span></Draggable>
-    </Container>
-  </div>
+  <v-tabs
+    show-arrows
+    centered
+    color="cyan"
+    dark
+    icons-and-text
+    @change="doTab"
+  >
+    <v-tabs-slider color="yellow"></v-tabs-slider>
+
+    <v-tab href="#inserisci">
+      Inserisci i nomi <v-icon>group_add</v-icon>
+    </v-tab>
+
+    <v-tab href="#chiama">
+      Chiama! <v-icon>record_voice_over</v-icon>
+    </v-tab>
+
+    <v-tab href="#info">
+      Informazioni <v-icon>info</v-icon>
+    </v-tab>
+
+    <v-tab-item value="inserisci">
+      <v-container>
+        <v-textarea v-model="names" placeholder="Scrivi qui i nomi dei bambini, uno per linea" autofocus clearable auto-grow></v-textarea>
+      </v-container>
+    </v-tab-item>
+
+    <v-tab-item value="chiama">
+      <v-container>
+      <v-layout justify-space-around align-space-around>
+      <v-flex xs4>
+      <v-card><v-card-title>Da chiamare</v-card-title><v-card-text>
+        <Container group-name="names" @drop="rap => onDrop(0, rap)" :get-child-payload="itemIndex => getChildPayload(0, itemIndex)">
+          <Draggable v-for="item in toBeCalled" :key="item.id"><v-chip color="green">{{item.name}}</v-chip></Draggable>
+        </Container>
+      </v-card-text></v-card> 
+      </v-flex>
+      <v-flex xs2>
+        <v-btn @click="doCall" alig>Chiama!</v-btn>
+      </v-flex>
+      <v-flex xs4>
+      <v-card><v-card-title>Chiamati</v-card-title><v-card-text>
+        <Container group-name="names" @drop="rap => onDrop(1, rap)" :get-child-payload="itemIndex => getChildPayload(1, itemIndex)">
+          <Draggable v-for="item in called" :key="item.id"><v-chip color="red">{{item.name}}</v-chip></Draggable>
+        </Container>
+      </v-card-text></v-card>
+      </v-flex>
+      </v-layout>
+      </v-container>
+    </v-tab-item>
+
+    <v-tab-item value="info">
+      <v-container>
+        Info… 
+      </v-container>
+    </v-tab-item>
+
+  </v-tabs>
 </template>
 
 <script>
@@ -25,18 +72,22 @@ export default {
     called: []
   }},
   methods: {
-    doCall() {
-      if (this.toBeCalled.length > 0) {
-        const toCall = this.toBeCalled.pop()
-        this.called.push(toCall)
+    doTab(name) {
+      if (name == 'chiama') {
+        this.toBeCalled = []
+        this.names.split(/\r?\n/).forEach((item, idx) => {
+          if (item) this.toBeCalled.push({id: idx, name: item})
+        })
+        this.called = []
       }
     },
-    doStart() {
-      this.toBeCalled = []
-      this.names.split(/\r?\n/).forEach((item, idx) => {
-        if (item) this.toBeCalled.push({id: idx, name: item})
-      })
-      this.called = []
+    doCall() {
+      const n = this.toBeCalled.length
+      if (n > 0) {
+        const idx = Math.floor(Math.random() * n)
+        this.called.push(this.toBeCalled[idx])
+        this.toBeCalled.splice(idx, 1)
+      }
     },
     onDrop(idx, {removedIndex, addedIndex, payload}) {
       if (removedIndex !== null) {
@@ -53,14 +104,9 @@ export default {
         else
           this.called.splice(addedIndex, 0, payload)
       }
-      // console.log(idx, removedIndex, addedIndex, payload.id)
     },
     getChildPayload(groupIndex, itemIndex) {
-      console .log('get-child-payload', groupIndex, itemIndex)
-      if (groupIndex == 0)
-        return this.toBeCalled[itemIndex]
-      else
-        return this.called[itemIndex]
+      return groupIndex == 0 ? this.toBeCalled[itemIndex] : this.called[itemIndex]
     },
   }
 }
